bf919d58399f52e84456ba621579c5f75f35df21
Glenn Posadas <hello@glennvon.com>
2020-11-19 01:56:25 +0800

    save progress

--- Memorize/Model/MemoryGame.swift
+++ Memorize/Model/MemoryGame.swift
@@ -11,6 +11,15 @@ struct MemoryGame<T> {
     
     var cards: Array<Card>
     
+    init(numberOfPairOfCards: Int) {
+        self.cards = [Card]()
+        
+        for pair in 0..<numberOfPairOfCards {
+            let newCard = Card(isFaceUp: <#T##Bool#>, isMatched: <#T##Bool#>, content: <#T##T#>)
+            self.cards.append(newCard)
+        }
+    }
+    
     func choose(_ card: Card) {
         
     }
--- Memorize/ViewModel/EmojiMemoryGame.swift
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -8,16 +8,22 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-  
-  // MARK: - Properties
-  
-  private(set) var model: MemoryGame<String>
-  
-  // MARK: - Intents
-  
-  func choose(_ card: MemoryGame<String>.Card) {
-    self.model.choose(card)
-  }
-  
-  // MARK: Functions
+    
+    // MARK: - Properties
+    
+    private var model: MemoryGame<String>
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        self.model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        self.model.choose(card)
+    }
+    
+    // MARK: Functions
 }
