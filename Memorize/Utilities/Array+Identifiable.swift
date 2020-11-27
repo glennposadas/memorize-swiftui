bc52fe28fd1e204e4a3d3fd9d44350b0004e21ec
Glenn Posadas <hello@glennvon.com>
2020-11-27 23:14:29 +0800

    optional matching first index.

--- Memorize/Utilities/Array+Identifiable.swift
+++ Memorize/Utilities/Array+Identifiable.swift
@@ -8,13 +8,13 @@
 import Foundation
 
 extension Array where Element: Identifiable {
-    func firstIndex(matching: Element) -> Int {
+    func firstIndex(matching: Element) -> Int? {
         for (index, currentItem) in self.enumerated() {
             if matching.id == currentItem.id {
                 return index
             }
         }
         
-        return -1
+        return .none
     }
 }
